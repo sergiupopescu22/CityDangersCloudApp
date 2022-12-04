4a6a33df47c6900cfbbac36cdc6f481b57f3d2c7
Sergiu Popescu (Nokia) <sergiu.popescu@nokia.com>
2022-12-05 00:12:18 +0200

    created the first pop up with google map

--- source/UI/city_dangers_ui/src/App.js
+++ source/UI/city_dangers_ui/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+// import { MapContainer } from './components/Map';
+// import Map from './components/Map';
+
+import { GoogleMap, useLoadScript} from "@react-google-maps/api";
+
+
 
 function App() {
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDZGjpMJAwwhKrMcage3StKjWARlN9R9ZY",
+  });
+
+  if (!isLoaded) 
+    return <div>Loading...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <div className='container'>
+    <h1>SA MOARA REACTUL CIPRIANE CA IMI PUSCA CAPUL</h1>
+    <div>
+      <GoogleMap 
+      zoom={10} 
+      center={{lat:45.757086, lng:21.228389}} 
+      mapContainerClassName="map-container">
+
+      </GoogleMap>
     </div>
+  </div>
   );
+
 }
 
 export default App;
